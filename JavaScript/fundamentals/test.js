d32009e26f2d2b0a140ed904fede8bc3bc96c695
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-02 17:08:20 -0400

    updating info

--- JavaScript/fundamentals/test.js
+++ JavaScript/fundamentals/test.js
@@ -1,4 +1,10 @@
+// ADDING TO REPOSITORY
+// git branch -M main
+// git remote add origin https://github.com/frutata/MERN_coding_dojo.git
+// git push -u origin main
 
+
+// UPDATING
 // git add .
 // git commit -m "some message here";
 // git push
