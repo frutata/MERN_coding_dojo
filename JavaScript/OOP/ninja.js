43feebb551d6c0c76c69fcfd9f90e01466dd7f30
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-03 13:24:35 -0400

    finished super ninja assignment

--- JavaScript/OOP/ninja.js
+++ JavaScript/OOP/ninja.js
@@ -27,8 +27,29 @@ class Ninja{
 
 }
 
-let Frutata = new Ninja("JP", 150);
+class Sensei extends Ninja{
+    constructor(nameInput){
+        super(nameInput, 200);
+        this.widsom = 10;
+        this.speed = 10;
+        this.strength = 10;
+    }
 
-Frutata.sayName().showStats().drinkSake();
+    speakWisdom(){
+        this.drinkSake();
+        let wisdom = `Sensei ${this.name} says: "What one programmer can do in one month, two programmers can do in two months."`;
+        console.log(wisdom);
+        return this;
+    }
+}
+
+let Frutata = new Ninja("JP", 150);
+let Splinter = new Sensei("Splinter");
+
+Splinter.speakWisdom().showStats();
+
+// console.log(Splinter);
+
+// Frutata.sayName().showStats().drinkSake();
 
 // console.log(Frutata);
