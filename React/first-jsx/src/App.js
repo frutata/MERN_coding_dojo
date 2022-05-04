7e465d0779c56857facc52d5fb99e7a9c135d3bd
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-04 13:25:22 -0400

    first jsx

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <ul>
+        <li>Learn React</li>
+        <li>Win VCT</li>
+        <li>Get Super Flexible</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
