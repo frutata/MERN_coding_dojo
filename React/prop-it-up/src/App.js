334dba128bf0b0f251ec85b7ca4fd28f89a9a4e6
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-04 14:35:43 -0400

    finished prop it up assignment

---
+++ React/prop-it-up/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import PersonCard from "./components/PersonCard";
+
+function App() {
+  return (
+    <div className="App">
+
+      <PersonCard lastName="Doe" firstName="Jane" age={45} hairColor="Black"/>
+      <PersonCard lastName="Smith" firstName="John" age={88} hairColor="Brown"/>
+      <PersonCard lastName="Fillmore" firstName="Millard" age={50} hairColor="Brown"/>
+      <PersonCard lastName="Smith" firstName="Maria" age={62} hairColor="Brown"/>
+
+
+    </div>
+  );
+}
+
+export default App;
