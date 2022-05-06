d7c707ab372b2a87f064e12cf34b9fef0214f4cf
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-06 14:04:08 -0400

    updated forms assignment

--- React/hook-form-practice/src/components/ProfileForm.jsx
+++ React/hook-form-practice/src/components/ProfileForm.jsx
@@ -7,6 +7,7 @@ const ProfileForm = () => {
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState("");
     let [confirm, setConfirm] = useState("");
+
     
     return (
 
@@ -14,28 +15,46 @@ const ProfileForm = () => {
 
             <div className='container'>
 
-                <div className='gray padding margin border'>
+                <div className='gray padding margin'>
                     <h1>Create Your Account</h1>
                     <form>
                         <div className='form-group container'>
                             <label htmlFor="">First Name:</label>
                             <input type="text" className='form-control' onChange={(e)=>setFirst(e.target.value)}/>
+                            {
+                                firstName.length<2? <p className='test-danger'>First name must be at LEAST two characters!</p> : null
+                            }
                         </div>
                         <div className='form=group container'>
                             <label htmlFor="">Last Name:</label>
                             <input type="text" className='form-control' onChange={(e)=>setLast(e.target.value)}/>
+                            {
+                                lastName.length<2? <p className='test-danger'>Last name must be at LEAST two characters!</p> : null
+                            }
                         </div>
                         <div className='form=group container'>
                             <label htmlFor="">Email:</label>
                             <input type="email" className='form-control' onChange={(e)=>setEmail(e.target.value)}/>
+                            {
+                                email.length<5? <p className='test-danger'>Email must be at LEAST five characters!</p> : null
+                            }
                         </div>
                         <div className='form=group container'>
                             <label htmlFor="">Password:</label>
                             <input type="password" className='form-control' onChange={(e)=>setPassword(e.target.value)}/>
+                            {
+                                password.length<8? <p className='test-danger'>Password must be at LEAST eight characters!</p> : null
+                            }
+                            {
+                                password != confirm? <p className='test-danger'>Password and Confirmation MUST match!</p> : null
+                            }
                         </div>
                         <div className='form=group container'>
                             <label htmlFor="">Confirm Password:</label>
                             <input type="password" className='form-control' onChange={(e)=>setConfirm(e.target.value)}/>
+                            {
+                                confirm.length<8? <p className='test-danger'>Confirmation must be at LEAST eight characters!</p> : null
+                            }
                         </div>
                     </form>
                 </div>
@@ -44,20 +63,51 @@ const ProfileForm = () => {
 
                 <div className='gray padding margin border'>
                     <h4>Your Account Data</h4>
-                    <div className='flex container'>
-                        <div className='left'>
-                            <p>First Name</p>
-                            <p>Second Name</p>
-                            <p>Email</p>
-                            <p>Password</p>
-                            <p>Confirm</p>
-                        </div>
-                        <div className='right'>
+                    <div className='container'>
+
+                        <div className='flex'>
+                            <div className='left'>
+                                <p>First Name</p>
+                            </div>
+                            <div className='right'>
                                 <p>{firstName}</p>
+                            </div>
+                        </div>
+
+                        <div className='flex'>
+                            <div className='left'>
+                                <p>Last Name</p>
+                            </div>
+                            <div className='right'>
                                 <p>{lastName}</p>
+                            </div>
+                        </div>
+
+                        <div className='flex'>
+                            <div className='left'>
+                                <p>Email</p>
+                            </div>
+                            <div className='right'>
                                 <p>{email}</p>
+                            </div>
+                        </div>
+
+                        <div className='flex'>
+                            <div className='left'>
+                                <p>Password</p>
+                            </div>
+                            <div className='right'>
                                 <p>{password}</p>
+                            </div>
+                        </div>
+                        
+                        <div className='flex'>
+                            <div className='left'>
+                                <p>Confirmation</p>
+                            </div>
+                            <div className='right'>
                                 <p>{confirm}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
