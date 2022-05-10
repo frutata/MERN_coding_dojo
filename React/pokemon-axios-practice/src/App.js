ff903d76f292143c281b8ddc4aaabdd0575b0e7c
Jonathan Pacheco <jp.pacheco19@gmail.com>
2022-05-10 15:03:24 -0400

    updated both pokemon assignments

---
+++ React/pokemon-axios-practice/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Pokemon from './components/Pokemon';
+
+function App() {
+    return (
+        <div className="App">
+            <Pokemon/>
+        </div>
+    );
+}
+
+export default App;
--- React/pokemon-axios-practice/src/components/Pokemon.jsx
+++ React/pokemon-axios-practice/src/components/Pokemon.jsx
@@ -1,40 +1,35 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
-const Pokemon = () => {
+const Pokemon = ()=>{
 
     // creating a state variable to store the response from the api fetch
     const [namesList, setNamesList] = useState([]);
 
     // function to grab the names of pokemon from the api
     const getPoke = ()=>{
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0")
-        .then(response => {
-            return response.json();
-        })
-        .then(response =>{
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0")
+        .then(response=>{
             console.log(response);
-            setNamesList(response.results);
-            // console.log(namesList, response.data);
+            setNamesList(response.data.results);
         })
-        .catch(err =>{
+        .catch(err=>{
             console.log(err);
         })
     }
 
-    return (
+    return(
         <>
-
         
-        <button onClick={getPoke}>Fetch All Pokemon</button>
-
+        <button onClick={getPoke}>Get Pokemon Names</button>
+        
         {
             namesList.map((names, idx)=>{
                 return(
                     // displaying the information we stored in our state variable by using the map function to loop through the hashmap
                     <div key={idx}>
-                        <h4>{names.name}</h4>
+                        <h3>{names.name}</h3>
                     </div>
-                    
                 )
             })
         }
